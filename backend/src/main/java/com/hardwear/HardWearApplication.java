7fc8f04f3f4a6d0ee74e8ee6a2986d6759a91cc6
dragosremetea <dragos.remetea@gmail.com>
2022-03-31 17:25:02 +0300

    BE initializr
    BE structure + DB connection

---
+++ backend/src/main/java/com/hardwear/HardWearApplication.java
@@ -0,0 +1,13 @@
+package com.hardwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardWearApplication.class, args);
+	}
+
+}
