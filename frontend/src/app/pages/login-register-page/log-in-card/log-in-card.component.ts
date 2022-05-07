7358823ae8fe59ead2008bce92abbe1fee5e7299
GramescuDan <gramescudan@yahoo.com>
2022-05-08 01:51:42 +0300

    Postam si noi ce avem

--- frontend/src/app/pages/login-register-page/log-in-card/log-in-card.component.ts
+++ frontend/src/app/pages/login-register-page/log-in-card/log-in-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../../services/user-service";
 import {firstValueFrom} from "rxjs";
+import {IUser} from "../../../models/user";
+import {GlobalVars} from "../../../services/global-vars";
 
 @Component({
   selector: 'app-log-in-card',
@@ -8,27 +10,36 @@ import {firstValueFrom} from "rxjs";
   styleUrls: ['./log-in-card.component.css']
 })
 export class LogInCardComponent implements OnInit {
+  user: IUser ={
+    email: "",
+    first_name: "",
+    id: 0,
+    last_name: "",
+    localdate: new Date,
+    password: "",
+    phone: "",
+    role: "",
+    username: ""
+  };
+
+  hide: boolean =true;
 
   constructor(private readonly _user:UserService) { }
-  Username = '';
-  Password = '';
-  hide: boolean =true;
+
+
   ngOnInit(): void {
   }
   onKey(event:any, input:number):void{
     if(input==0){
-      this.Username=event.target.value;
+      this.user.username=event.target.value;
     }
     else{
-      this.Password=event.target.value;
+      this.user.password=event.target.value;
     }
   }
 
-  async loginpress():Promise<void>{
-    let message = {
-      useranme: this.Username,
-      password: this.Password
-    }
-    await firstValueFrom(this._user.post(message));
+  loginpress():void{
+
+    this._user.post(this.user).subscribe(data => GlobalVars.user = data);
   }
 }
