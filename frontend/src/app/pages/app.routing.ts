0e0a11c7c2992425c98576d7584c2eb15af04817
GramescuDan <gramescudan@yahoo.com>
2022-05-08 20:31:02 +0300

    login Routing

--- frontend/src/app/pages/app.routing.ts
+++ frontend/src/app/pages/app.routing.ts
@@ -4,11 +4,18 @@ import {MyAccountPageComponent} from "./my-account-page/my-account-page.componen
 import {MainPageComponent} from "./main-page/main-page.component";
 import {NotFoundPageComponent} from "./not-found-page/not-found-page.component";
 import {LoginRegisterPageComponent} from "./login-register-page/login-register-page.component";
+import {LogInCardComponent} from "./login-register-page/log-in-card/log-in-card.component";
+import {RegisterCardComponent} from "./login-register-page/register-card/register-card.component";
+import {MainCardComponent} from "./login-register-page/main-card/main-card.component";
 
 const routes: Routes = [
   {path:'my-account', component: MyAccountPageComponent},
-  {path:'', component:MainPageComponent},
-  {path:'login', component:LoginRegisterPageComponent},
+  {path:'main', component:MainPageComponent},
+  {path:'login', component:LoginRegisterPageComponent, children:[
+      {path:'',component:MainCardComponent},
+      {path:'login', component:LogInCardComponent},
+      {path:'register', component:RegisterCardComponent}
+    ]},
   {path:"**", component:NotFoundPageComponent}
 
 ];
