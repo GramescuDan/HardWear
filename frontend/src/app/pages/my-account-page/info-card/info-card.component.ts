39f60813879a1770ee1713db6fbe6e757e7c17eb
GramescuDan <gramescudan@yahoo.com>
2022-05-12 01:09:09 +0300

    work on the card

--- frontend/src/app/pages/my-account-page/info-card/info-card.component.ts
+++ frontend/src/app/pages/my-account-page/info-card/info-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-info-card',
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoCardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
+  @Input() headername: String;
+  @Input() fitext: String;
+  @Input() setext: String;
+  @Input() thtext: String;
   ngOnInit(): void {
   }
 
