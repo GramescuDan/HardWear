ab13fc97c6b9534ec2b12ec48913b7b7204e74eb
GramescuDan <gramescudan@yahoo.com>
2022-05-19 00:41:41 +0300

    styled dialogs

--- frontend/src/app/pages/my-account-page/my-account-dialog/my-account-dialog.component.ts
+++ frontend/src/app/pages/my-account-page/my-account-dialog/my-account-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-account-dialog',
@@ -8,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class MyAccountDialogComponent implements OnInit {
 
   constructor() { }
+
+  @Input() fitext: string;
+  @Input() fires: string;
+
+  @Input() setext: string;
+  @Input() seres: string;
+
+  @Input() thtext: string;
+  @Input() thres:string;
+
+
+
 
   ngOnInit(): void {
   }
