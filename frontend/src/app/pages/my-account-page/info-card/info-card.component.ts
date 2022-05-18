6399869a069899c1f080d0b2707b177af8b00579
GramescuDan <gramescudan@yahoo.com>
2022-05-19 00:55:54 +0300

    nice things can happen sometimes

--- frontend/src/app/pages/my-account-page/info-card/info-card.component.ts
+++ frontend/src/app/pages/my-account-page/info-card/info-card.component.ts
@@ -37,5 +37,6 @@ export class InfoCardComponent implements OnInit {
     }
 
     });
+
   }
 }
--- frontend/src/app/pages/my-account-page/my-account-dialog/my-account-dialog.component.ts
+++ frontend/src/app/pages/my-account-page/my-account-dialog/my-account-dialog.component.ts
@@ -10,19 +10,20 @@ export class MyAccountDialogComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  fitext: string = this.data.fitext;
-  fires: string = this.data.fires;
+  fitext: string = this.data.var1;
+  fires: string = this.data.var1val;
 
-  setext: string = this.data.setext;
-  seres: string = this.data.seres;
+  setext: string = this.data.var2;
+  seres: string = this.data.var2val;
 
-  thtext: string = this.data.thtext;
-  thres:string = this.data.thres;
+  thtext: string = this.data.var3;
+  thres:string = this.data.var3val;
 
 
 
 
   ngOnInit(): void {
+    console.log(this.data);
   }
 
 }
