58bda9d3bdb49a349704d4e2a5437b6f89dacec8
GramescuDan <gramescudan@yahoo.com>
2022-05-20 22:24:32 +0300

    working forms

--- frontend/src/app/pages/my-account-page/my-account-dialog/my-account-dialog.component.ts
+++ frontend/src/app/pages/my-account-page/my-account-dialog/my-account-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, Input, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {AuthSerivce} from "../../../services/auth-serivce";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {IUser} from "../../../models/user";
 
 @Component({
   selector: 'app-my-account-dialog',
@@ -10,7 +11,8 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class MyAccountDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private _auth:AuthSerivce,private formBuilder:FormBuilder) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private _auth: AuthSerivce, private formBuilder: FormBuilder) {
+  }
 
   fitext: string = this.data.var1;
   fires: string = this.data.var1val;
@@ -19,31 +21,38 @@ export class MyAccountDialogComponent implements OnInit {
   seres: string = this.data.var2val;
 
   thtext: string = this.data.var3;
-  thres:string = this.data.var3val;
+  thres: string = this.data.var3val;
 
-  select:boolean = this.data.selector;
+  select: boolean = this.data.selector;
 
   updateForm: FormGroup;
 
   ngOnInit(): void {
-this.updateForm = this.formBuilder.group(
-  {
-    fir: [],
-    sec: [],
-    th: []
-  }
-)
-  }
+    this.updateForm = this.formBuilder.group(
+      {
+        fir: this.fires,
+        sec: this.seres,
+        th: this.thres
+      })
 
-  pressSave(){
-  if(this.select){
-    //this is not working
-    this._auth.curentUserValue.username ;
-
-  } else{
-
+    this.updateForm.valueChanges.subscribe()
   }
 
+  pressSave() {
+    let user: IUser;
+    user = this._auth.curentUserValue;
+    if (this.select) {
+      //Post
+      user.username = this.updateForm.get('fir')?.value;
+      user.email = this.updateForm.get('sec')?.value;
+      user.phone = this.updateForm.get('th')?.value;
+
+    } else {
+      user.firstName = this.updateForm.get('fir')?.value;
+      user.lastName = this.updateForm.get('sec')?.value;
+      user.location = this.updateForm.get('th')?.value;
+    }
+    this._auth.update(user);
   }
 
-  }
+}
