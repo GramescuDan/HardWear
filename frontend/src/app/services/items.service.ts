392ac38d4116b97a46699836378f8caf489a7b90
Dan Gramescu <dan.gramescu@amdaris.com>
2022-06-02 12:25:39 +0300

    mock out

--- frontend/src/app/services/items.service.ts
+++ frontend/src/app/services/items.service.ts
@@ -23,35 +23,6 @@ const apiUrl = environment.apiUrl + 'items';
 })
 export class ItemsService {
   items$ = new BehaviorSubject<Item[]>([]);
-  mock: Item[] = [
-    {
-      id: 1,
-      name: 'Some item',
-      price: 12,
-      quantity: 13,
-      description: 'Some description',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg',
-      categories: ['Laptops'],
-    },
-    {
-      id: 2,
-      name: 'Some item 2',
-      price: 12,
-      quantity: 13,
-      description: 'Some description 2',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg',
-      categories: ['Laptops'],
-    },
-    {
-      id: 3,
-      name: 'Some item 3',
-      price: 12,
-      quantity: 13,
-      description: 'Some description 3',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg',
-      categories: ['Laptops'],
-    },
-  ];
 
   constructor(
     private readonly _http: HttpClient,
@@ -68,7 +39,7 @@ export class ItemsService {
     if (this._categoryService.categories.length == 0) {
       return this._http.get<Item[]>(`${environment.apiUrl}items`).pipe(
         tap(console.log),
-        map(items => (items.length ? items : this.mock)),
+
         tap(items => this.itemsUpdate(items)),
         shareReplay(),
       );
@@ -86,7 +57,7 @@ export class ItemsService {
   async add(item: Item, file: Blob) {
     let formData = new FormData();
     formData.append('currentFile', file);
-    formData.append('item', new Blob([JSON.stringify(item)], { type: 'application/json' }));
+    formData.append('item', new Blob([JSON.stringify(item)], {type: 'application/json'}));
     return firstValueFrom(
       this._http.post(apiUrl, formData).pipe(tap(console.log), this.#reloadItemsAfterRequest()),
     );
